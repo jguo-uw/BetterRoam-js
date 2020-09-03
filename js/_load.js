53882efba86e67adfb0d2ec4fe7ad224c21e957f
Jiajie Guo <jguo16@uw.edu>
2020-09-03 13:14:43 -0700

    Fix missing addElementToPage func

--- js/_load.js
+++ js/_load.js
@@ -7,6 +7,14 @@ function addCSSToPage(tagId, cssToAdd) {
 addElementToPage(Object.assign(document.createElement('link'),{href:cssToAdd, rel: 'stylesheet'} ) , tagId, 'text/css')
 }
 
+function addElementToPage(element, tagId, typeT ) {
+    try { document.getElementById(tagId).remove() } catch(e){}  //Delete any existing reference
+    if(disabledFeatures && disabledFeatures.indexOf(tagId) > -1) { return } //Exit if disabled
+    Object.assign(element, { type:typeT, async:false, tagId:tagId } )
+    document.getElementsByTagName('head')[0].appendChild(element)  
+}
+
+
 const UserScriptServer =  'https://roam_plus.imfast.io/'
 
 //load all custom files 

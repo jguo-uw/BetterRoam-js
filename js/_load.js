6f18376585e904628bddb7cc1b4242809c3964dc
Jiajie Guo <jguo16@uw.edu>
2020-09-03 13:10:52 -0700

    Fix missing addCSSToPage func

--- js/_load.js
+++ js/_load.js
@@ -1,10 +1,16 @@
 /* global danceMonkey, loadKeyEvents, toastr, loadTypeAhead, displayHelp */
+function addModuleToPage(tagId, script) {
+addElementToPage(Object.assign(document.createElement('script'),{src:script}) , tagId, 'module')
+}
+
+function addCSSToPage(tagId, cssToAdd) {
+addElementToPage(Object.assign(document.createElement('link'),{href:cssToAdd, rel: 'stylesheet'} ) , tagId, 'text/css')
+}
 
 const UserScriptServer =  'https://roam_plus.imfast.io/'
 
 //load all custom files 
-    addCSSToPage( 'myRMCSS',                UserScriptServer + 'css/styleRM.css'          )
     addCSSToPage( 'mySplitCSS',             UserScriptServer + 'css/split_screen.css'     )
     addCSSToPage( 'myBetterCSS',            UserScriptServer + 'css/better_roam.css'      )
-addScriptToPage( 'mySortedAttrTable',       UserScriptServer + 'js/sortAttrTable.js'      )
-addScriptToPage( 'myPomoBell',              UserScriptServer + 'js/PomoBell.js'           )
+addModuleToPage( 'SortedAttrTable',       UserScriptServer + 'js/sortAttrTable.js'      )
+addModuleToPage( 'PomoBell',              UserScriptServer + 'js/PomoBell.js'           )
